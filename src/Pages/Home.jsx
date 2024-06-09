7d2b52d170fa60383d9b7c0f20281a65f649c981
annv <nva030801@gmail.com>
2024-06-09 12:55:21 +0700

    wip

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -53,7 +53,7 @@ const Home = () => {
                 {/*<div className="container mx-auto bg-white rounded-t-3xl px-4 pt-3 md:px-10 md:pt-8 custom-font border-b-2 text-center capitalize text-blue-400 pb-2 md:pb-4">*/}
                 {/*    <h1>{content.name}</h1>*/}
                 {/*</div>*/}
-                <div className="container mx-auto bg-white rounded-3xl px-4 py-5 md:px-10 md:py-8 custom-font ck-content" dangerouslySetInnerHTML={{ __html: products.content }}></div>
+                <div className="container mx-auto bg-white rounded-3xl px-4 py-5 md:px-10 md:py-8 custom-font ck-content" dangerouslySetInnerHTML={{ __html: content.content }}></div>
             </div>
         </div>
     )
